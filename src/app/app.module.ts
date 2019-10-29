49658882e6f3d6258c9d61f0170bc7a7e374abcc
Michael Colburn <michaelco@brc.local>
2019-10-29 12:23:34 -0500

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,7 +8,8 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+	NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
